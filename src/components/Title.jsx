c5081b2bde41f09a24abcb7a0948c1aadac4edc8
Thiago Souza <thiago29.06@hotmail.com>
2022-09-01 14:17:22 -0300

    treinando 3 e 4

---
+++ src/components/Title.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Title extends Component {
+  render() {
+    console.log(this);
+    const { headline } = this.props;
+    return (
+      <div>
+        <h2>{headline}</h2>
+      </div>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
